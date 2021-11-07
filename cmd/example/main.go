ab8174a0db35b3df33df9233e5ad13eac693d299
paulsumskoy <p.sumskoy@gmail.com>
2021-11-07 14:36:02 +0200

    Fake error

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -1,58 +1,62 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "os"
-  "io"
-  "strings"
-  lab2 "github.com/Destaby/architecture-lab-2"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	//"os"
+	//"io"
+	"strings"
+
+	lab2 "github.com/Destaby/architecture-lab-2"
 )
 
 var (
-  inputExpression = flag.String("e", "", "Expression to compute")
-  inputFile       = flag.String("f", "", "File")
-  outputFile      = flag.String("o", "", "Output")
+	inputExpression = flag.String("e", "", "Expression to compute")
+	inputFile       = flag.String("f", "", "File")
+	outputFile      = flag.String("o", "", "Output")
 )
 
 func main() {
-  flag.Parse()
+	flag.Parse()
 
-  fmt.Println("inputExpression", *inputExpression)
-  fmt.Println("inputFile", *inputFile)
-  fmt.Println("outputFile", *outputFile)
+	fmt.Println("inputExpression", *inputExpression)
+	fmt.Println("inputFile", *inputFile)
+	fmt.Println("outputFile", *outputFile)
 
-  var err error
+	var err error
 
-  var fileOrExpression io.Reader
-  if *inputFile != "" {
-    fileOrExpression, err = os.Open(*inputFile)
-    if err != nil {
-      panic(err)
-    }
-  } else {
-    fileOrExpression = strings.NewReader(*inputExpression)
-  }
+	var fileOrExpression io.Reader
+	if *inputFile != "" {
+		fileOrExpression, err = os.Open(*inputFile)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		fileOrExpression = strings.NewReader(*inputExpression)
+	}
 
-  // TODO
+	// TODO
 
-  var out io.Writer
-  var outF *os.File
+	var out io.Writer
+	var outF *os.File
 
-  if *outputFile != "" {
-    outF, err = os.OpenFile(*outputFile, os.O_WRONLY|os.O_CREATE, 0600)
-    out = outF
-    if err != nil {
-      panic(err)
-    }
-  } else {
+	if *outputFile != "" {
+		outF, err = os.OpenFile(*outputFile, os.O_WRONLY|os.O_CREATE, 0600)
+		out = outF
+		if err != nil {
+			panic(err)
+		}
+	} else {
 		out = os.Stdout
-  }
- 
-  handler := &lab2.ComputeHandler{fileOrExpression, out}
-  err = handler.Compute()
+	}
 
-  if (err != nil) {
-    io.Copy(os.Stderr, strings.NewReader(err.Error()))
-  }
+	handler := &lab2.ComputeHandler{fileOrExpression, out}
+	err = handler.Compute()
+
+	if err != nil {
+		io.Copy(os.Stderr, strings.NewReader(err.Error()))
+	}
 }

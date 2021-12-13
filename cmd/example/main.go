4df401cf9cb0fbb4b9ea3b2c334cc9f832750817
Destaby <rudik.nick.nick@gmail.com>
2021-12-13 12:34:44 +0200

    should close file after opening

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -38,10 +38,9 @@ func main() {
 	var out io.Writer
 	var outF *os.File
 
-	defer outF.Close()
-
 	if *outputFile != "" {
 		outF, err = os.OpenFile(*outputFile, os.O_WRONLY|os.O_CREATE, 0600)
+		defer outF.Close()
 		out = outF
 		if err != nil {
 			panic(err)
